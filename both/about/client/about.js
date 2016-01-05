ef4dd32f986a7271cd948630334387f467a4c00b
Devin Dodson <ddodson5@gmail.com>
2016-01-04 18:28:56 -0800

    Initial Commit.

---
+++ both/about/client/about.js
@@ -0,0 +1,23 @@
+
+/*
+ *   Project Name: Instructent
+ *   User: Devin Dodson
+ *   Created: 9/23/2015
+ *   Notes:
+ */
+
+/* -------- Route --------- */
+var about_route = Instructent.Client.Router.Routes.about = {name: "about", path: "/about"};
+Router.route(about_route.path, {name: about_route.name, template: about_route.name
+});
+
+Template.about.helpers({
+
+
+});
+
+Template.about.events({
+
+
+});
+
